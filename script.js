2c90fe965b8be7b52d3981e7d4177ff8b52dad65
wiksa <wiktoriaa.ma99@gmail.com>
2026-07-13 18:23:07 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+aconst places=[
+"Teleporter",
+"Mały Wulkan",
+"Duży Wulkan",
+"Plac"
+];
+
+const app=document.getElementById("timers");
+
+places.forEach(place=>{
+
+const section=document.createElement("div");
+section.className="section";
+
+section.innerHTML=`<h2>${place}</h2>`;
+
+for(let i=1;i<=5;i++){
+
+const timer=document.createElement("div");
+
+timer.className="timer";
+
+timer.innerHTML=`
+<div class="channel">CH${i}</div>
+<div class="time">--:--</div>
+`;
+
+timer.onclick=()=>{
+
+const minutes=prompt("Podaj liczbę minut");
+
+if(minutes===null)return;
+
+alert(`Tutaj uruchomimy timer na ${minutes} minut.`);
+
+};
+
+section.appendChild(timer);
+
+}
+
+app.appendChild(section);
+
+});
